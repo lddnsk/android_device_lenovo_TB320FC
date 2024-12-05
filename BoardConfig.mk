585847e11c4adf69243ead7150fa1b12d945f975
lddnsk <lddnsk@gmail.com>
2024-12-05 16:49:18 +0700

    Build number b0004

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,9 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 # Encryption
 BOARD_USES_METADATA_PARTITION := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
-PLATFORM_VERSION := 14
+# Version number must be larger than system's version, Or FBE decryption should fail.
+# e.g. When PLATFORM_VERSION is 14 and system's android version is 15, decryption will fail because of upgrade required error.
+PLATFORM_VERSION := 99
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
@@ -106,13 +108,9 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 	vendor.display.config@1.0 \
 	vendor.display.config@2.0
 
-# Security patch level
-#VENDOR_SECURITY_PATCH := 2021-08-01
-
 # Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 14
+#PLATFORM_SECURITY_PATCH := 2099-12-31
+#VENDOR_SECURITY_PATCH := 2099-12-31
 
 # Extras
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -145,7 +143,7 @@ TW_INCLUDE_FASTBOOTD := true
 TW_EXCLUDE_APEX := true
 #TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_NO_EXFAT_FUSE := true
-TW_DEFAULT_BRIGHTNESS := 100
+TW_DEFAULT_BRIGHTNESS := 614
 TW_HAS_EDL_MODE := true
 TW_SKIP_ADDITIONAL_FSTAB := true
 
